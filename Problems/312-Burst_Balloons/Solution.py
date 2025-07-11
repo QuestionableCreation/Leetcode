01157f0fa781c41985248bb1633f9031f5a4f867
Ethan Bain <166454988+QuestionableCreation@users.noreply.github.com>
2025-07-11 16:02:48 +0200

    usure what changed

--- Problems/312-Burst_Balloons/Solution.py
+++ Problems/312-Burst_Balloons/Solution.py
@@ -1,49 +1,14 @@
 class Solution:
+
     def maxCoins(self, nums: List[int]) -> int:
-        def Pop(nums:List[int],index:int):
-            num1, num2= getNumbers(nums,index)
-            coins=num1*num2*nums.pop(index)
-            return coins
-        
-        
-        def getNumbers(nums:List[int],index:int): 
-            if index-1<0:
-                num1=1
-            else:
-                num1=nums[index-1]
-            if index+1>=len(nums):
-                num2=1
-            else:
-                num2=nums[index+1]
-            return num1, num2
-            
-            
-        coins=0
-        biggest=0
-        i=0
-        while i< len(nums):
-        
-            if (nums[i]==1):
-                coins+=Pop(nums,i)
-            else:
-                if nums[i]>nums[biggest]:
-                    biggest=i
-                i+=1
-        
-        while nums:
-            if biggest==0:
-                biggest=1
-            coins+=Pop(nums,biggest-1)
-            biggest-=1
-        
-        return coins
-            
-        
 
+        n = len(nums)
+        score = [1] + nums + [1] 
+        dp = [ [0] * (n + 2) for _ in range(n + 2) ]
 
-# Main method to run the code
-if __name__ == "__main__":
-    sol = Solution()
-    nums = [3, 1, 5, 8]  # Example input
-    result = sol.maxCoins(nums)
-    print("Maximum coins:", result)
+        for j in range(n + 2):
+            for i in range(j)[::-1]: 
+                for k in range(i + 1, j):
+                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + score[i] * score[k] * score[j])
+
+        return dp[0][n + 1]
